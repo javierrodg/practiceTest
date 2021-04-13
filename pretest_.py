78fcc865733f312c52b4d8ae71e2f928551cee77
Javier Rodriguez <javiersmac@Javierss-Mac-mini.local>
2021-04-13 11:18:02 -0700

    finish structured english

---
+++ pretest_.py
@@ -0,0 +1,16 @@
+
+
+"""
+The program is trying to determine which payment option is better(more money).
+Fist option is 100 dollars per day for 10 days. The second option is 1 dollar
+a day with it beiing doubled each day for 10 days. There will be two functions to calculate the pay rate.
+function 1 will calculate the amount for the first option, function 2 will calculate the amount for the second options.
+
+function1 will output 100 * 10 days.
+function 2 will loop 10 times, with each time, doubling the amount and add the amount to the total 
+
+if the amount is equal, we output tp the user option 1 and option 2 pays the same
+if option 1 is better, we output option 1 is better 
+if option 1 is better, we output option 1 is better 
+
+"""
