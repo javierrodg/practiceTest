7224c135266a3919145540c3c85864ef664cfb21
Javier Rodriguez <javiersmac@Javierss-Mac-mini.local>
2021-04-13 11:46:02 -0700

    finish pseudocode

--- pretest_.py
+++ pretest_.py
@@ -14,3 +14,29 @@ if option 1 is better, we output option 1 is better
 if option 1 is better, we output option 1 is better 
 
 """
+
+"""
+# option 1
+    return 100 * 10
+
+# option 2
+    amount = 1
+    list1 = []
+    loop 10 times
+        add amount to list1
+        amount *=2 
+    sum = sum of all items in loop
+    return amount
+# main
+    var1 = option1
+    var2 = option2
+
+    if var1 = var2
+        "option 1 and option 2 pays the same"
+    if var1 < var2
+        "option 1 is better
+    else
+        if the option2 is better, we output to "option 2 is better"    
+
+main
+"""
